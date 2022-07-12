72b8a5a4e4b56406f4e867ee140948e64660fd98
mabab <max22dev@gmail.com>
2022-07-12 13:00:36 +0300

    fix capabilities

--- app/helpers/consume-context.js
+++ app/helpers/consume-context.js
@@ -1,1 +1,1 @@
-export { default, contextConsumer } from '@alexlafroscia/ember-context/helpers/consume-context';
+export { default, contextConsumer } from '@allright-com/ember-context/helpers/consume-context';
